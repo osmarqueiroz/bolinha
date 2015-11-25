a04ad3c80f13c105770827341b95def0890262d8
osmar <osmar@solucoesindustriais.com.br>
2015-11-25 02:14:15 -0200

    ajuste da logica da direcao

--- script/estrutura.js
+++ script/estrutura.js
@@ -31,10 +31,8 @@ var ModificarPosicao = {
         var posicaoBaseX = bolinha.posicao.posicaoX;
         var posicaoBaseY = bolinha.posicao.posicaoY;
         var radiano = radiano(bolinha.posicao.angulo);
-
         bolinha.posicao.posicaoX = ModificarPosicao.calcularPosicaoX(radiano, passo, posicaoBaseX);
         bolinha.posicao.posicaoY = ModificarPosicao.calcularPosicaoY(radiano, passo, posicaoBaseY);
-
         return bolinha;
     },
     calcularPosicaoX: function (radiano, passo, posicaoRef) {
@@ -42,36 +40,64 @@ var ModificarPosicao = {
     },
     calcularPosicaoY: function (radiano, passo, posicaoRef) {
         return Math.sin(radiano) * passo + posicaoRef;
-    },
+    }
 }
 
-var VerificarColisao = function (origemX, origemY, width, height) {
-    this.origemMinX = origemX;
-    this.origemMinY = origemY;
-    this.origemMaxX = origemX + width;
-    this.origemMaxY = origemY + height;
-    
-    function cenario(bolinha) {
+var VerificarColisao = {
+    origemMinX: 0,
+    origemMinY: 0,
+    origemMaxX: 0,
+    origemMaxY: 0,
+    init: function (origemX, origemY, width, height) {
+        VerificarColisao.origemMinX = origemX;
+        VerificarColisao.origemMinY = origemY;
+        VerificarColisao.origemMaxX = origemX + width;
+        VerificarColisao.origemMaxY = origemY + height;
+    },
+    cenario: function (bolinha) {
         var posicaoX = bolinha.posicao.posicaoX;
         var posicaoY = bolinha.posicao.posicaoY;
-
-        if ((posicaoX <= this.origemMinX || posicaoX >= this.origemMaxX) &&
-                (posicaoY >= this.origemMinY && posicaoY <= this.origemMaxY))
+        if ((posicaoX <= VerificarColisao.origemMinX || posicaoX >= VerificarColisao.origemMaxX) &&
+                (posicaoY >= VerificarColisao.origemMinY && posicaoY <= VerificarColisao.origemMaxY))
         {
-            return true;
+            return VerificarColisao.alterarAnguloHorizontal(bolinha);
         }
-        if ((posicaoY <= this.origemMinY || posicaoY >= this.origemMaxY) &&
-                (posicaoX >= this.origemMinX && posicaoX <= this.origemMaxX))
+        if ((posicaoY <= VerificarColisao.origemMinY || posicaoY >= VerificarColisao.origemMaxY) &&
+                (posicaoX >= VerificarColisao.origemMinX && posicaoX <= VerificarColisao.origemMaxX))
         {
-            return true;
+            return VerificarColisao.alterarAnguloVertical(bolinha);
         }
-        return false;
-    }
-    
-    return {
-        cenario: cenario,
-        alterarAngulo: function (bolinha) {
-
+        return bolinha;
+    },
+    alterarAnguloHorizontal: function (bolinha) {
+        // 2 1 1 2
+        // 4 3 3 4
+        var grau = bolinha.posicao.angulo;
+        var resultado = 0;
+        resultado = Math.abs(180 - grau);
+        if (grau > 180) {
+            resultado = Math.abs(360 - resultado);
         }
+        bolinha.posicao.angulo = resultado;
+        return bolinha;
+    },
+    alterarAnguloVertical: function (bolinha) {
+        // 4 1 1 4 
+        // 3 2 2 3
+        var grau = bolinha.posicao.angulo;
+        var anguloGlobal = (parseInt(grau / 90)+1)*90;
+        
+        var resultado = 0;
+        
+        resultado = Math.abs(anguloGlobal - grau);
+        
+        if (grau > 180) {
+            resultado = Math.abs(90 + resultado);
+        }else{
+            resultado = resultado + 180;
+        }
+        bolinha.posicao.angulo = resultado;
+        return bolinha;
     }
 }
+
